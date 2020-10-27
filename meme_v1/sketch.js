53a4624c0e481a150bd869d7352b7d9077c9a32d
thisiscj12 <chantell.jeffrey@stu.bmcc.cuny.edu>
2020-10-27 19:42:50 -0400

    adds image example

--- meme_v1/sketch.js
+++ meme_v1/sketch.js
@@ -6,28 +6,36 @@
 
 var title = 'Honeymoon Studios'
 var description ='An animation company made to make everyone’s dreams come true. Here we aim to give the world more diverse stories for children by giving opportunities for those children to tell their stories. Our teams’ main goal is to give minority children a chance to see more people like them on their screens. To feel more connected to the characters that they will grow to love.n Inclusivity is very important to us and we strive to make that our mission in the animation industry.'
+var honeymoonLogo;
 
+function preload(){
+	honeymoonLogo = loadImage('honeymoonlogo.jpg')
+}
 
 
  function setup(){
  	createCanvas(740, 460)
  	/*canvas.drawingContext.miterLimit = 2;
  	used for stroke*/
+ 	imageMode(CENTER)
  }
 
 
 
 function draw(){
 	background('#F79765');
-	textSize(60);
-	fill('#70442E');
+//image//
+	image(honeymoonLogo, width/2, height/2 - 100, 250, 250)
+	
 
 //visualized alignment//
 	/*strokeWeight(1);
 	line(width/2, 0, width/2, height)
 	line(0, height/2, width, height/2)*/
 	
-
+//text//
+	textSize(60);
+	fill('#70442E');
 	textStyle(ITALIC)
 	textFont('Courier New');
 	textAlign(CENTER, TOP);
